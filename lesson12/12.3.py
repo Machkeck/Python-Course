aaa32f745e76de7f7b9616da722ccd05c982ef63
Maciek Wlazlo <w.maciek@hotmail.com>
2017-01-21 14:26:36 +0100

    12.3 fixed original limits

--- lesson12/12.3.py
+++ lesson12/12.3.py
@@ -9,20 +9,23 @@ def create_list(n, k):
     return L
 
 def mediana_sort(L, left=0, right=0):
-    M = sorted(L)
-    #print("M="+str(M))
-    length = len(L)
+    originalLength = len(L)
     if left < 0:
         left = 0
-    if right > length:
-        right = length-1
+    if right > originalLength-1:
+        right = originalLength-1
     if right < left:
         raise ValueError("left index greater than right index")
 
-    if ((right-left)+1) %2 == 0:
-        return (M[int(((right-left)-1)/2)+left],M[int(((right-left)-1)/2+1)+left])
+
+    M = sorted(L[left:(right+1)])
+    #print("M="+str(M))
+    length = len(M)
+
+    if (length) %2 == 0:
+        return (M[int((length-1)/2)],M[int((length-1)/2+1)])
     else:
-        return M[int((right-left)/2)+left]
+        return M[int((length)/2)]
 
 L = create_list(10,100)
 print("L = "+str(L))
@@ -31,6 +34,7 @@ print("Mediana[0,9] = "+str(mediana_sort(L,0,len(L)-1)))
 print("Mediana[0,2] = "+str(mediana_sort(L,0,2)))
 print("Mediana[2,3] = "+str(mediana_sort(L,2,3)))
 print("Mediana[2,4] = "+str(mediana_sort(L,2,4)))
+print("Mediana[2,7] = "+str(mediana_sort(L,2,7)))
 print("Mediana[2,29] = "+str(mediana_sort(L,2,29)))
 print("Mediana[-1,19] = "+str(mediana_sort(L,-1,19)))
 try:
